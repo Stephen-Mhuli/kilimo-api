e634274a12452f1f267fee964189945b7569378a
Stephen Mhuli <stevemshible@gmail.com>
2022-03-30 06:25:40 +0300

    laravel api getting started

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'role_id' => '1',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('@admin'),
+        ]);
+        DB::table('users')->insert([
+            'role_id' => '2',
+            'name' => 'Customer',
+            'email' => 'customer@gmail.com',
+            'password' => Hash::make('@customer'),
+        ]);
+        DB::table('users')->insert([
+            'role_id' => '3',
+            'name' => 'Seller',
+            'email' => 'seller@gmail.com',
+            'password' => Hash::make('@seller'),
+        ]);
+    }
+}
